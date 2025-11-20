c57ccb893b232d7434b07ea73bd0b146cd00b5d4
andre <millankiiy@gmail.com>
2025-11-20 18:37:29 -0500

    Se crean todos los gateways administrador,Cliente,Prestamo,Solicitud y Vendedor

---
+++ prestamo/prestamo/src/main/java/com/prestamigos/prestamo/domain/gateway/AdministradorGateway.java
@@ -0,0 +1,6 @@
+package com.prestamigos.prestamo.domain.gateway;
+
+public interface AdministradorGateway {
+
+    boolean existeAdministrador(Long administradorId);
+}
---
+++ prestamo/prestamo/src/main/java/com/prestamigos/prestamo/domain/gateway/PrestamoGateway.java
@@ -0,0 +1,21 @@
+package com.prestamigos.prestamo.domain.gateway;
+
+import com.prestamigos.prestamo.domain.model.Prestamo;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PrestamoGateway {
+
+    Prestamo guardarPrestamo(Prestamo prestamo);
+
+    Optional<Prestamo> buscarPorId(Long id);
+
+    List<Prestamo> listarTodos();
+
+    List<Prestamo> listarPorVendedorId(Long vendedorId);
+
+    Prestamo actualizarPrestamo(Prestamo prestamo);
+
+    void eliminarPrestamo(Long id);
+}
